c35eb5cd05b60414ad0dc071c247b5b3ee21b2c8
v-Sharan <vsharansago@gmail.com>
2023-07-01 14:57:24 +0530

    navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className="navbar">
       <Link to="/">Home</Link>
       <Link to="/events">Events</Link>
       <Link to="/newEvent">Add new Event</Link>
-    </div>
+    </nav>
   );
 };
 
